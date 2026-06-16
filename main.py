b4479856a0ffa77f5bfb306f030a9a34476ab6a9
a33publicores <a33publicores@gmail.com>
2026-06-16 17:35:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
+import json
+import os
 import flet as ft
 import views.codigo as codigo
+from views.jukebox import jukebox_view
 
 
 def main(page: ft.Page):
 
     page.title = "PlayBar GO"
 
-    page.expand=True
+    page.window_width = 450
     page.window_height = 850
+
+    if os.path.exists("data/session.json"):
+
+        try:
+
+            with open(
+                "data/session.json",
+                "r",
+                encoding="utf-8"
+            ) as f:
+
+                session = json.load(f)
+
+            jukebox_view(
+                page,
+                session["codigo"],
+                session["cliente"],
+                session["telefono"],
+                session["logo"]
+            )
+
+            return
+
+        except:
+            pass
 
     codigo.codigo_view(page)
 
 
-ft.app(
-    target=main,
+ft.run(
+    main,
     assets_dir="assets"
 )
