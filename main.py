e4ad7c03a1467775ac6f8525fe619bedfaf4f728
a33publicores <a33publicores@gmail.com>
2026-06-24 21:05:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import json
-import os
 import flet as ft
 
 from views.splash import splash_view
@@ -13,16 +11,17 @@ def main(page: ft.Page):
     page.window_width = 450
     page.window_height = 850
 
-    codigo = page.session.get("codigo")
+    # Verificar si existe una sesión guardada
+    if page.session.store.contains_key("codigo"):
 
-    if codigo:
+        codigo = page.session.store.get("codigo")
 
         jukebox_view(
             page,
             codigo,
-            page.session.get("cliente"),
-            page.session.get("telefono"),
-            page.session.get("logo")
+            page.session.store.get("cliente"),
+            page.session.store.get("telefono"),
+            page.session.store.get("logo")
         )
 
         return
